b77cdd06527404d1794f50aafef0696707937497
徐朋飞 <pengfei.xu@sensoro.com>
2019-10-15 18:06:56 +0800

    添加唯一id生成函数

--- library.js
+++ library.js
@@ -280,7 +280,11 @@ const Library = {
         return data.sort(fn);
     },
     // ***文件编码
-
+    // ...
+    // 生成不重复ID
+    createRandomId() {
+        return (Math.random() * 10000000).toString(16).substr(0, 4) + '-' + (new Date()).getTime() + '-' + Math.random().toString().substr(2, 5);
+    }
 };
 
 export default Library;
